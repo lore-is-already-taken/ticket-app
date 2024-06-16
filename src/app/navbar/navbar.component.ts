4148e80292a262452cd6f8f817ac19941dd31f45
lore <lore@gmail.com>
2024-06-16 16:09:46 -0400

    dunno what is

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,17 +3,16 @@ import { RouterLink, RouterOutlet } from "@angular/router";
 import { AuthService } from "../core/services/auth.service";
 
 @Component({
-    selector: "app-navbar",
-    standalone: true,
-    imports: [RouterLink, RouterOutlet],
-    templateUrl: "./navbar.component.html",
-    styleUrl: "./navbar.component.css",
+	selector: "app-navbar",
+	standalone: true,
+	imports: [RouterLink, RouterOutlet],
+	templateUrl: "./navbar.component.html",
+	styleUrl: "./navbar.component.css",
 })
 export class NavbarComponent {
-    constructor(private authService: AuthService) { }
-    logout() {
-        this.authService.logout();
-    }
-    isAuthenticated = this.authService.isAuthenticated()
-
+	constructor(private authService: AuthService) {}
+	logout() {
+		this.authService.logout();
+	}
+	isAuthenticated = this.authService.isAuthenticated();
 }
